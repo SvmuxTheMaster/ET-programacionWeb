9c3663cff2d34ae4f20df3185347e2253d1eb48e
Samuel Segovia <sam.segovia@duocuc.cl>
2024-07-15 05:16:01 -0400

    Implementacion de JS II

---
+++ app/static/app/js/modal.js
@@ -0,0 +1,31 @@
+// Obtener el modal
+var modal = document.getElementById("productModal");
+
+// Obtener el <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en <span> (x), cerrar el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic fuera del modal, cerrarlo
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Función para abrir el modal y mostrar los detalles del producto
+function openModal(productId) {
+    var servicio = document.querySelector(`[data-id='${productId}']`);
+    var title = servicio.querySelector('.card-title').innerText;
+    var imageSrc = servicio.querySelector('img').src;
+    var price = servicio.querySelector('.precio').innerText;
+    
+    document.getElementById('modalTitulo').innerText = title;
+    document.getElementById('modalImg').src = imageSrc;
+    document.getElementById('modalPrecio').innerText = price;
+
+    modal.style.display = "block";
+}
